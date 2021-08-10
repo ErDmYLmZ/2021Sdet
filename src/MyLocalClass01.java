014abe861779ff366ed96a1e5cf522dd07a7ac71
ErDmYLmZ <erdemyilmaz4@gmail.com>
2021-08-10 18:06:04 +0200

    second attempt

--- src/MyLocalClass01.java
+++ src/MyLocalClass01.java
@@ -3,5 +3,6 @@ import org.w3c.dom.ls.LSOutput;
 public class MyLocalClass01 {
     public static void main(String[] args) {
         System.out.println("Branch practice");
+        System.out.println("second attempt");
     }
 }
